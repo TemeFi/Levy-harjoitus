8b46c29c6b4cd237e6d56492490c58c4d60cfe93
TemeFi <teemukarhunen95@gmail.com>
2021-11-10 19:52:41 +0200

    Valmis

---
+++ Levyt Koulu Harjoitus/Levyt/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Levyt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Teemu Karhunen
+            int dvdmaara = 0;
+            int brmaara = 0;
+            int mdmaara = 0;
+            int kokomaara = 0;
+
+            int dvdhinta = 0;
+            int brhinta = 0;
+            int mdhinta = 0;
+            int kokohinta = 0;
+
+            string vastaus;
+
+            Console.WriteLine("Ostatko DVD levyjä? 6 Euroa per levy. Y/N?");
+            vastaus = Console.ReadLine();
+            vastaus = vastaus.ToUpper();
+
+            if (vastaus == "Y")
+            {
+                Console.WriteLine("Montako levyä ostat?");
+                dvdmaara = int.Parse(Console.ReadLine());
+                dvdhinta = 6 * dvdmaara;
+
+            }
+
+            Console.WriteLine("Ostatko BluRay levyjä?  10 Euroa per levy.  Y/N?");
+
+            vastaus = Console.ReadLine();
+            vastaus = vastaus.ToUpper();
+
+            if (vastaus == "Y")
+            {
+                Console.WriteLine("Montako levyä ostat?");
+                brmaara = int.Parse(Console.ReadLine());
+                brhinta = 10 * brmaara;
+            }
+
+            Console.WriteLine("Ostatko MiniDisk levyjä?  8 Euroa per levy.  Y/N?");
+
+            vastaus = Console.ReadLine();
+            vastaus = vastaus.ToUpper();
+
+            if (vastaus == "Y")
+            {
+                Console.WriteLine("Montako levyä ostat?");
+                mdmaara = int.Parse(Console.ReadLine());
+                mdhinta = 8 * mdmaara;
+
+            }
+            kokomaara = dvdmaara + brmaara + mdmaara;
+            kokohinta = dvdhinta + brhinta + mdhinta;
+
+            Console.WriteLine("Ostit " + kokomaara + " levyä");
+            Console.WriteLine("Näistä " + dvdmaara + " oli DVD levyjä, " + brmaara + " BluRay levyjä ja " + mdmaara + " MiniDisk levyjä.");
+            Console.WriteLine("DVD hinnaksi tuli " + dvdhinta + " euroa. BluRay hinnaksi " + brhinta + " euroa. MiniDisk hinnaksi " + mdhinta + " euroa.");
+            Console.WriteLine("Kokonais hinta on " + kokohinta + " euroa.");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Levyt Koulu Harjoitus/Levyt/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Levyt")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Levyt")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6eee4a42-57e5-4ac8-9111-c8c673c1dcbc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
